e9b1b8dcc4532d4938dec682c0d83c2a665e0fe5
Gary Martin <gjm@apache.org>
2012-04-01 09:23:22 +0000

    Theme code import: Quick create ticket cancel button
    git-svn-id: https://svn.apache.org/repos/asf/incubator/bloodhound/trunk@1308054 13f79535-47bb-0310-9956-ffa450edef68

--- bloodhound_theme/bhtheme/htdocs/scripts/theme.js
+++ bloodhound_theme/bhtheme/htdocs/scripts/theme.js
@@ -4,5 +4,12 @@ $( function () {
     // Do not close dropdown menu if user clicks on form controls
     $('.dropdown-menu input, .dropdown-menu label')
         .click(function (e) { e.stopPropagation(); });
+    
+    // Install quick create box click handlers
+    $('#qct-cancel').click(
+        function() {
+          $('#qct-box input').val('');
+        }
+      )
   })
 
