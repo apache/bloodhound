7361592804b6aedf67ebbe0ce08ed80f44425dad
Gary Martin <gjm@apache.org>
2012-04-01 09:26:31 +0000

    Theme code import: Ticket fields inserted in quick create ticket menu
    git-svn-id: https://svn.apache.org/repos/asf/incubator/bloodhound/trunk@1308056 13f79535-47bb-0310-9956-ffa450edef68

--- bloodhound_theme/bhtheme/htdocs/scripts/theme.js
+++ bloodhound_theme/bhtheme/htdocs/scripts/theme.js
@@ -2,7 +2,7 @@
 
 $( function () {
     // Do not close dropdown menu if user clicks on form controls
-    $('.dropdown-menu input, .dropdown-menu label')
+    $('.dropdown-menu input, .dropdown-menu label, .dropdown-menu select')
         .click(function (e) { e.stopPropagation(); });
     
     // Install quick create box click handlers
--- bloodhound_theme/bhtheme/theme.py
+++ bloodhound_theme/bhtheme/theme.py
@@ -16,13 +16,84 @@
 #  specific language governing permissions and limitations
 #  under the License.
 
-from genshi.core import Stream
+from genshi.builder import tag
+from genshi.core import Markup, Stream, TEXT
+from genshi.filters import Transformer
+from genshi.input import HTML
 from trac.core import *
+from trac.ticket.model import Ticket
+from trac.ticket.web_ui import TicketModule
+from trac.web.api import Request, IRequestFilter, ITemplateStreamFilter
+from trac.web.chrome import Chrome
+from trac.web.main import RequestDispatcher
 
-from themeengine.api import IThemeProvider, ThemeBase
+from themeengine.api import ThemeBase
+
+from urlparse import urlparse
+from wsgiref.util import setup_testing_defaults
+
+def dummy_request(env, uname=None):
+    environ = {}
+    setup_testing_defaults(environ)
+    environ.update({
+                'REQUEST_METHOD' : 'GET',
+                'SCRIPT_NAME' : urlparse(str(env._abs_href())).path,
+                'trac.base_url' : str(env._abs_href()), 
+                })
+    req = Request(environ, lambda *args, **kwds: None)
+    # Intercept redirection
+    req.redirect = lambda *args, **kwds: None
+    # Setup user information
+    if uname is not None :
+      environ['REMOTE_USER'] = req.authname = uname
+    
+    rd = RequestDispatcher(env)
+    chrome = Chrome(env)
+    req.callbacks.update({
+        'authname': rd.authenticate,
+        'chrome': chrome.prepare_request,
+        'hdf': getattr(rd, '_get_hdf', None),
+        'locale' : getattr(rd, '_get_locale', None),
+        'perm': rd._get_perm,
+        'session': rd._get_session,
+        'tz': rd._get_timezone,
+        'form_token': rd._get_form_token
+    })
+    return req
 
 class BloodhoundTheme(ThemeBase):
-    """Look and feel of Bloodhound issue tracker."""
-    
+    """Look and feel of Bloodhound issue tracker.
+    """
     template = htdocs = css = screenshot = True
-    
+
+    # Internal methods
+    def is_theme_active(self, req):
+        # TODO: Implement
+        return True
+
+class QuickCreateTicketDialog(Component):
+    implements(IRequestFilter)
+
+    # IRequestFilter(Interface):
+
+    def pre_process_request(self, req, handler):
+        """Nothing to do.
+        """
+        return handler
+
+    def post_process_request(self, req, template, data, content_type):
+        """Append necessary ticket data
+        """
+        if (template, data, content_type) != (None,) * 3: # TODO: Check !
+            if data is None:
+                data = {}
+            fakereq = dummy_request(self.env)
+            ticket = Ticket(self.env)
+            tm = TicketModule(self.env)
+            tm._populate(fakereq, ticket, False)
+            fields = dict([f['name'], f] \
+                        for f in tm._prepare_fields(fakereq, ticket))
+            data['qct'] = { 'fields' : fields }
+        return template, data, content_type
+
+
