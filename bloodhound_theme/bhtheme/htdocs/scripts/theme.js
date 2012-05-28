d78cf0016bfbd68b4a2830077714aea2eee758b5
Gary Martin <gjm@apache.org>
2012-05-28 17:02:47 +0000

    theme: implements quick ticket confirmations - #86 (from olemis)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/bloodhound/trunk@1343320 13f79535-47bb-0310-9956-ffa450edef68

--- bloodhound_theme/bhtheme/htdocs/scripts/theme.js
+++ bloodhound_theme/bhtheme/htdocs/scripts/theme.js
@@ -1,17 +1,41 @@
 
 
 $( function () {
-    //$('#qct-newticket').popover({});
-    
+    var qct_result = {};
+    var qct_timeout = null;
+
     // Do not close dropdown menu if user interacts with form controls
     $('.dropdown-menu input, .dropdown-menu label, .dropdown-menu select')
         .click(function (e) { e.stopPropagation(); });
 
-    /* Helper functions */
+    // Install popover for create ticket shortcut
+    // Important: Further options specified in markup
+    $('#qct-newticket').popover({
+        title : function () {
+            ticket = qct_info.ticket;
+            if (ticket)
+              title = 'Ticket #' + qct_info.ticket;
+            else
+              title = 'Error creating ticket';
+            return title + ' <a class="close" id="qct-alert-close" ' +
+                'data-dismiss="alert" href="#">&times;</a>'
+          },
+        content : function () { return qct_info.msg; }
+      });
+    $('body').on('click.close', '#qct-alert-close', 
+        function (e) { qct_alert_close() });
 
-    // Display message triggered by quick create box
+    // Display & hide message triggered by quick create box
     function qct_alert(msg) {
-      alert(msg);
+      qct_info = msg;
+      jQuery('#qct-newticket').popover('show');
+      if (qct_timeout)
+        clearTimeout(qct_timeout);
+      qct_timeout = setTimeout(qct_alert_close, 4000);
+    }
+
+    function qct_alert_close() {
+      jQuery('#qct-newticket').popover('hide');
     }
 
     // Clear input controls inside quick create box
@@ -27,10 +51,16 @@ $( function () {
       );
     $('#qct-create').click(
         function() {
-          var qct_url = $('#qct-create').attr('data-target');
-          $.post(qct_url, $('#qct-form').serialize(), 
+          var base_url = $('#qct-create').attr('data-target');
+          $.post(base_url + '/qct', $('#qct-form').serialize(), 
               function(ticket_id) {
-                qct_alert('Created ticket ' + ticket_id);
+                qct_alert({
+                    ticket: ticket_id,
+                    msg: '<span class="alert alert-success" ' +
+                          ' style="padding:3px"> Has been created</span>' +
+                        '</span> <a href="' + base_url + '/ticket/' +
+                        ticket_id + '" class="pull-right">View / Edit</a>'
+                  });
               })
               .error(function(jqXHR, textStatus, errorMsg) {
                   var msg = 'Error:' + errorMsg;
@@ -40,7 +70,11 @@ $( function () {
                     msg = 'Could not create ticket . Error : ' + errorMsg;
                   else if (textStatus === 'abort')
                     msg = 'Aborted request'
-                  qct_alert(msg);
+                  qct_alert({ 
+                      ticket : null, 
+                      msg : '<span class="alert alert-error"' +
+                          ' style="display:block">' + msg + '</span>'
+                    });
                 });
           qct_clearui();
         }
