d7e5864b390114720a8692c6602c3556fda87d01
Jure Žitnik <jure@apache.org>
2013-02-18 15:32:08 +0000

    #355, patch renderer test cases, patch t355_r1446579_trac_test_mimeview_patch.diff applied (from Olemis)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/bloodhound/branches/bep_0003_multiproduct@1447328 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bloodhound_multiproduct/tests/mimeview/patch.py
@@ -0,0 +1,67 @@
+
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+
+"""Tests for Apache(TM) Bloodhound's patch renderer in product environments"""
+
+import unittest
+
+from trac.mimeview.api import Mimeview
+from trac.mimeview.patch import PatchRenderer
+from trac.mimeview.tests.patch import PatchRendererTestCase
+from trac.web.chrome import Chrome
+
+from multiproduct.env import ProductEnvironment
+from tests.env import MultiproductTestCase
+
+class ProductPatchRendererTestCase(PatchRendererTestCase, MultiproductTestCase):
+
+    @property
+    def env(self):
+        env = getattr(self, '_env', None)
+        if env is None:
+            self.global_env = self._setup_test_env(
+                    enable=[Chrome, PatchRenderer]
+                )
+            self._upgrade_mp(self.global_env)
+            self._setup_test_log(self.global_env)
+            self._load_product_from_data(self.global_env, self.default_product)
+            self._env = env = ProductEnvironment(
+                    self.global_env, self.default_product)
+        return env
+
+    @env.setter
+    def env(self, value):
+        pass
+
+    def setUp(self):
+        PatchRendererTestCase.setUp(self)
+        self.patch = Mimeview(self.env).renderers[0]
+
+    def tearDown(self):
+        self.global_env.reset_db()
+        self.global_env = self._env = None
+
+
+def test_suite():
+    return unittest.TestSuite([
+            unittest.makeSuite(ProductPatchRendererTestCase,'test'),
+        ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
+
