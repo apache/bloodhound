de9ce9d78c45c0f44bd1eae3a36f982dfca79698
Matevz Bradac <matevz@apache.org>
2013-03-12 08:25:34 +0000

    #429 - Multiproduct UI: Ticket page (fixed ticket preview)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/bloodhound/branches/bep_0003_multiproduct@1455436 13f79535-47bb-0310-9956-ffa450edef68

--- bloodhound_theme/bhtheme/htdocs/js/theme.js
+++ bloodhound_theme/bhtheme/htdocs/js/theme.js
@@ -74,7 +74,9 @@ $( function () {
       );
     $('#qct-create').click(
         function() {
-          var base_url = $('#qct-create').attr('data-target') + '/' + $('#field-product').val();
+          var base_url = $('#qct-create').attr('data-target');
+          if (base_url === '/')
+            base_url = '';
           $.post(base_url + '/qct', $('#qct-form').serialize(), 
               function(ticket_id) {
                 qct_alert({
