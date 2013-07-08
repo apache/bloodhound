6680e7340acd6bd5775b036d3f61c8c2587e6bd3
Ryan Ollos <rjollos@apache.org>
2013-07-08 06:31:27 +0000

    Increment version to 0.7.0.
    git-svn-id: https://svn.apache.org/repos/asf/bloodhound/trunk@1500586 13f79535-47bb-0310-9956-ffa450edef68

--- bloodhound_dashboard/setup.py
+++ bloodhound_dashboard/setup.py
@@ -35,6 +35,7 @@ versions = [
     (0, 4, 0),
     (0, 5, 0),
     (0, 6, 0),
+    (0, 7, 0),
     ]
     
 latest = '.'.join(str(x) for x in versions[-1])
--- bloodhound_search/setup.py
+++ bloodhound_search/setup.py
@@ -32,6 +32,7 @@ versions = [
     (0, 4, 0),
     (0, 5, 0),
     (0, 6, 0),
+    (0, 7, 0),
     ]
 
 latest = '.'.join(str(x) for x in versions[-1])
