c3b34ca9aeea9b452815ca58a37e5b998e24791e
Antonia Horincar <ahorincar@apache.org>
2013-08-08 12:12:35 +0000

    Created jQuery function that displays the link to the iframe
    git-svn-id: https://svn.apache.org/repos/asf/bloodhound/branches/bep_0007_embeddable_objects@1511730 13f79535-47bb-0310-9956-ffa450edef68

--- bloodhound_embedding_plugin/bhembedding/ht_docs/iframe_generator.js
+++
@@ -1,8 +0,0 @@
-function makeIFrame(src)
-{
-   var iframe = document.createElement('iframe');
-   iframe.setAttribute("src", src);
-   iframe.style.width = 500+"px";
-   iframe.style.height = 500+"px";
-   document.body.appendChild(iframe);
-}
