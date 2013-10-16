apache/bloodhound

commit c469cc1b347a7987ebfee7441ef467231cda8b74

0.8dev: "Admin" entry is moved from the mainnav to metanav prior to template rendering. Refs #636.
git-svn-id: https://svn.apache.org/repos/asf/bloodhound/trunk@1522789 13f79535-47bb-0310-9956-ffa450edef68

bloodhound_theme/bhtheme/theme.py
@@ -249,7 +249,13 @@ class BloodhoundTheme(ThemeBase):
             return req.href.__call__("wiki", *a, **kw)
 
         req.href.wiki = hwiki
-        
+
+        # Move 'admin' entry from mainnav to metanav
+        for i, entry in enumerate(req.chrome['nav']['mainnav']):
+            if entry['name'] == 'admin':
+                req.chrome['nav']['metanav'] \
+                    .append(req.chrome['nav']['mainnav'].pop(i))
+
         return handler
 
     def post_process_request(self, req, template, data, content_type):

bloodhound_theme/bhtheme/templates/bloodhound_theme.html
@@ -60,8 +60,7 @@
             </a>
           </div>
           <!--! top menu (login, logout...) -->
-          <py:with vars="meta_items = dict([i.name, i] for i in chrome.nav.metanav);
-                         main_items = dict([i.name, i] for i in chrome.nav.mainnav);">
+          <py:with vars="meta_items = dict([i.name, i] for i in chrome.nav.metanav);">
             <div id="usermenu" class="span8">
               <div class="metanav pull-right">
                 <span id="nav_login" py:choose="">
@@ -77,8 +76,8 @@
                 <span py:if="'logout' in meta_items" id="nav_logout">
                   ${meta_items.logout.label}
                 </span>
-                <span py:if="'admin' in main_items">
-                  ${main_items.admin.label}
+                <span py:if="'admin' in meta_items">
+                  ${meta_items.admin.label}
                 </span>
                 <span py:if="'help' in meta_items">
                   ${meta_items.help.label}
@@ -216,23 +215,21 @@
           <!--! main navigation tabs -->
           <div class="span6">
             <ul py:if="chrome.nav.mainnav or chrome.nav.metanav"
-                class="nav nav-tabs pull-right"
-                id="mainnav"
+                class="nav nav-tabs pull-right" id="mainnav"
                 py:with="mainnav_show = ('wiki', 'tickets', 'browser');
-                          mainnav_to_meta = ('admin',);
-                          metanav_hide = ('login', 'logout', 'prefs', 'help', 'about', 'register')">
+                         metanav_hide = ('login', 'logout', 'prefs', 'help', 'about', 'register', 'admin')">
               <!--! TODO: Parameterize visible mainnav items in trac.ini -->
               <li py:if="chrome.nav.mainnav"
                   py:for="idx, item in enumerate(i for i in chrome.nav.mainnav if i.name in mainnav_show)" 
                   class="${classes(first_last(idx, chrome.nav.mainnav), active=item.active)}">${item.label}</li>
               <li class="dropdown ${all(not i.active for i in chrome.nav.mainnav if i.name in mainnav_show) and 'active' or None}"
                   py:if="len([i for i in chrome.nav.metanav if i.name not in metanav_hide] +
-                             [i for i in chrome.nav.mainnav if i.name not in mainnav_show + mainnav_to_meta])">
+                             [i for i in chrome.nav.mainnav if i.name not in mainnav_show])">
                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">More<b class="caret"></b></a>
                 <ul class="dropdown-menu">
                   <li py:if="chrome.nav.mainnav"
                       py:for="idx, item in enumerate(i for i in chrome.nav.mainnav
-                                                     if i.name not in mainnav_show + mainnav_to_meta)"
+                                                     if i.name not in mainnav_show)"
                       class="${classes(first_last(idx, chrome.nav.mainnav), active=item.active)}">${item.label}</li>
                   <py:if test="chrome.nav.metanav">
                     <li py:if="len([i for i in chrome.nav.metanav if i.name not in metanav_hide])"

installer/bloodhound_setup.py
@@ -77,7 +77,7 @@ BASE_CONFIG = {'components': {'bhtheme.*': 'enabled',
                'theme': {'theme': 'bloodhound',},
                'trac': {'mainnav': ','.join(['dashboard', 'wiki', 'browser',
                                              'tickets', 'newticket', 'timeline',
-                                             'roadmap', 'search', 'admin']),
+                                             'roadmap', 'search']),
                         'environment_factory': '',
                         'request_factory': '',},
                'project': {'footer': ('Get involved with '

commit f804809590482557155bb9f66c8e5c150a377027

0.8dev: Fixed regression in r1522789 which resulted in active mainnav item not being highlighted. Refs #693.
`req.chrome` should be manipulated in `post_process_request` rather than `pre_process_request` since it is assigned a dictionary in `trac.web.chrome:Chrome.prepare_request`.

Patch by Olemis Lang.


git-svn-id: https://svn.apache.org/repos/asf/bloodhound/trunk@1532641 13f79535-47bb-0310-9956-ffa450edef68

bloodhound_theme/bhtheme/theme.py
@@ -252,12 +252,6 @@ class BloodhoundTheme(ThemeBase):
 
         req.href.wiki = hwiki
 
-        # Move 'admin' entry from mainnav to metanav
-        for i, entry in enumerate(req.chrome['nav'].get('mainnav', [])):
-            if entry['name'] == 'admin':
-                req.chrome['nav'].setdefault('metanav', []) \
-                    .append(req.chrome['nav']['mainnav'].pop(i))
-
         return handler
 
     def post_process_request(self, req, template, data, content_type):
@@ -295,6 +289,12 @@ class BloodhoundTheme(ThemeBase):
         
         is_active_theme = is_active_theme()
         if self.disable_all_trac_css and is_active_theme:
+            # Move 'admin' entry from mainnav to metanav
+            for i, entry in enumerate(req.chrome['nav'].get('mainnav', [])):
+                if entry['name'] == 'admin':
+                    req.chrome['nav'].setdefault('metanav', []) \
+                       .append(req.chrome['nav']['mainnav'].pop(i))
+
             if self.disable_all_trac_css:
                 stylesheets = links.get('stylesheet', [])
                 if stylesheets: