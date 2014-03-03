apache/bloodhound

commit 773e68cfa0813c6d35985cd6648f8acafa3460b6

#727 Post QCT fields to preview new ticket. Disables "more" link if no product selected. Remove hidden fields not required for QCT 'more'.
git-svn-id: https://svn.apache.org/repos/asf/bloodhound/trunk@1570616 13f79535-47bb-0310-9956-ffa450edef68

bloodhound_theme/bhtheme/htdocs/bloodhound.css
@@ -342,6 +342,9 @@ ol.arabic     { list-style-type: decimal }
  margin-top: 5px;
  margin-bottom: 5px;
 }
+#qct-more {
+    visibility: hidden;
+}
 
 /*
   workaround for -hidden/-visible buttons in a btn-group, which don't

bloodhound_theme/bhtheme/templates/bloodhound_theme.html
@@ -122,7 +122,8 @@
                   <option py:for="idx,option in enumerate(field.options)"
                           py:with="description = field.options_desc[idx] if field.options_desc else option"
                           selected="${field.value == option or None}"
-                          value = "$option"
+                          value = "${option.value}"
+                          data-product-new-ticket-url = "${option.product_new_ticket_url}"
                           py:content="description"></option>
                   <optgroup py:for="optgroup in field.optgroups"
                             py:if="optgroup.options"
@@ -176,7 +177,7 @@
                             </div>
 
                             <py:for each="field in qct.hidden_fields">
-                              <input type="hidden" name="field_${field.name}" value="${field.value}" />
+                              <input class="qct-product-scope-extra" type="hidden" name="field_${field.name}" value="${field.value}" />
                             </py:for>
                           </form>
 

bloodhound_theme/bhtheme/theme.py
@@ -47,6 +47,7 @@ from bhdashboard.web_ui import DashboardModule
 from bhdashboard import wiki
 
 from multiproduct.env import ProductEnvironment
+from multiproduct.model import Product
 from multiproduct.web_ui import PRODUCT_RE, ProductModule
 from bhtheme.translation import _, add_domain
 
@@ -555,7 +556,7 @@ class QuickCreateTicketDialog(Component):
             dum_req.perm = req.perm
             ticket = Ticket(self.env)
             tm._populate(dum_req, ticket, False)
-            all_fields = dict([f['name'], f]
+            all_fields = dict([f['name'], self.add_prod_new_ticket_url(dum_req, f)]
                               for f in tm._prepare_fields(dum_req, ticket)
                               if f['type'] == 'select')
 
@@ -565,13 +566,14 @@ class QuickCreateTicketDialog(Component):
                 product_field['options'] = \
                     [prefix for prefix in product_field['options']
                      if req.perm.has_permission('TICKET_CREATE',
-                                                Neighborhood('product', prefix)
+                                                Neighborhood('product', prefix['value'])
                                                     .child(None, None))]
+
                 if self.env.product and \
                         self.env.product.prefix in product_field['options']:
                     product_field['value'] = self.env.product.prefix
                 product_field['options_desc'] = [
-                    ProductEnvironment.lookup_env(self.env, p).product.name
+                    ProductEnvironment.lookup_env(self.env, p['value']).product.name
                         for p in product_field['options']
                 ]
             else:
@@ -587,8 +589,7 @@ class QuickCreateTicketDialog(Component):
                 'fields': [all_fields[k] for k in self.qct_fields
                            if k in all_fields],
                 'hidden_fields': [all_fields[k] for k in all_fields.keys()
-                                  if k not in self.qct_fields]
-            }
+                                  if k not in self.qct_fields] }
         return template, data, content_type
 
     # IRequestHandler methods
@@ -664,6 +665,18 @@ class QuickCreateTicketDialog(Component):
                 self.log.exception("Failure sending notification on creation "
                                    "of ticket #%s: %s" % (t.id, e))
         return t['product'], t.id
+
+    def add_prod_new_ticket_url(self, req, fields):
+        if fields.get('name') == 'product':
+            options_with_attrs = []
+            for option in fields.get('options', []):
+                options_with_attrs.append({
+                    'value': option,
+                    'product_new_ticket_url': \
+                            req.href.products(option, 'newticket')
+                })
+            fields['options'] = options_with_attrs
+        return fields
 
 from pkg_resources import get_distribution
 application_version = get_distribution('BloodhoundTheme').version

bloodhound_multiproduct/multiproduct/ticket/web_ui.py
@@ -50,16 +50,11 @@ class ProductTicketModule(TicketModule):
         """Override for TicketModule process_request"""
         ticketid = req.args.get('id')
         productid = req.args.get('productid', '')
-        qct_product = req.args.get('product')  # From QCT form
+
         if not ticketid:
             # if /newticket is executed in global scope (from QCT), redirect
             # the request to /products/<first_product_in_DB>/newticket
-            #
-            # If here from "More fields" link then pass along QCT form fields.
-            # If product arg present then redirect to
-            # /products/<qct_product>/newticket .
-
-            if not productid and not qct_product and \
+            if not productid and \
                     not isinstance(self.env, ProductEnvironment):
                 default_product = self.env.config.get('ticket',
                                                       'default_product')
@@ -67,16 +62,8 @@ class ProductTicketModule(TicketModule):
                 prefixes = [prod.prefix for prod in products]
                 if not default_product or default_product not in prefixes:
                     default_product = products[0].prefix
-                req.redirect(req.href.products(default_product, 'newticket',
-                                               req.args))
-            elif qct_product:
-                # Most likely arrived here via the QCT "More fields" link.
-                # Use <qct_product> to redirect to the appropriate product
-                # scope
+                req.redirect(req.href.products(default_product, 'newticket'))
 
-                req.args.pop('product')  # don't reenter block on redirect.
-                req.redirect(req.href.products(qct_product, 'newticket',
-                                               req.args))
             return self._process_newticket_request(req)
 
         if req.path_info in ('/newticket', '/products'):

bloodhound_theme/bhtheme/htdocs/js/theme.js
@@ -94,15 +94,35 @@ $( function () {
       });
 
     /** 
-     * Pass QCT form fields to full ticket form when "More fields" is clicked
+     * POST QCT form fields to full ticket form when "More fields" is clicked
      */
     $('#qct-more').click(function(e) {
+        // As we're not creating the ticket, we'll remove hidden fields
+        // that result in unnecessary validation messages.
         e.preventDefault();
-        var href = $(this).attr('href');
-        var params = $('#qct-form').serialize();
-        params = params.replace(/field_/g, ''); // map to newticket querystring
-        window.location = href + '?' + params;
+        $qct_form = $('#qct-form');
+        $qct_form.unbind('submit');
+        new_ticket_url = $qct_form.find(':selected').attr('data-product-new-ticket-url');
+        $qct_form.attr('action', new_ticket_url);
+        $('.qct-product-scope-extra').remove();
+        $qct_form.append('<input type="hidden" value="1" name="preview" />');
+        $qct_form.submit();
+
     });
+
+    function set_qct_more_visibility(is_visible) {
+        if (is_visible) {
+            $('#qct-more').css('visibility', 'visible');
+        } else {
+            $('#qct-more').css('visibility', 'hidden');
+        }
+    }
+
+    $('#field-product').change(function(e) {
+        set_qct_more_visibility($(this).val());
+    });
+
+    set_qct_more_visibility($('#field-product').val());
 
     $('body').on('click.close', '#qct-alert-close', 
         function (e) { qct_alert_close() });

commit 4a7708bda53226d7758c40c6acf179bb87cdf00e

0.8dev: Fixed regression in r1570616: the product at scope is no longer the default selection. Refs #727.
The code in `theme.py` was also refactored to simplify preparation of the `options` dictionary.


git-svn-id: https://svn.apache.org/repos/asf/bloodhound/trunk@1573459 13f79535-47bb-0310-9956-ffa450edef68

bloodhound_theme/bhtheme/htdocs/bloodhound.css
@@ -343,7 +343,7 @@ ol.arabic     { list-style-type: decimal }
  margin-bottom: 5px;
 }
 #qct-more {
-    visibility: hidden;
+ visibility: hidden;
 }
 
 /*

bloodhound_theme/bhtheme/templates/bloodhound_theme.html
@@ -117,13 +117,15 @@
               <!--! create ticket button + dropdown -->
               <py:def function="field_select(field)">
                 <select id="field-${field.name}" name="field_${field.name}"
-                    class="input-block-level" data-field="${field.name}" data-optional="${'true' if field.optional else 'false'}">
+                        class="input-block-level" data-field="${field.name}"
+                        data-optional="${'true' if field.optional else 'false'}">
                   <option value="">${'' if field.optional else 'Choose...'}</option>
-                  <option py:for="idx,option in enumerate(field.options)"
-                          py:with="description = field.options_desc[idx] if field.options_desc else option"
-                          selected="${field.value == option or None}"
-                          value = "${option.value}"
-                          data-product-new-ticket-url = "${option.product_new_ticket_url}"
+                  <option py:for="option in field.options"
+                          py:with="description = option.description or option;
+                                   value = option.value or option"
+                          selected="${field.value == value or None}"
+                          value = "$value"
+                          data-product-new-ticket-url = "$option.new_ticket_url"
                           py:content="description"></option>
                   <optgroup py:for="optgroup in field.optgroups"
                             py:if="optgroup.options"

bloodhound_theme/bhtheme/theme.py
@@ -47,7 +47,6 @@ from bhdashboard.web_ui import DashboardModule
 from bhdashboard import wiki
 
 from multiproduct.env import ProductEnvironment
-from multiproduct.model import Product
 from multiproduct.web_ui import PRODUCT_RE, ProductModule
 from bhtheme.translation import _, add_domain
 
@@ -532,7 +531,7 @@ class QuickCreateTicketDialog(Component):
         locale_dir = pkg_resources.resource_filename(__name__, 'locale')
         add_domain(self.env.path, locale_dir)
         super(QuickCreateTicketDialog, self).__init__(*args, **kwargs)
-        
+
     # IRequestFilter(Interface):
 
     def pre_process_request(self, req, handler):
@@ -556,26 +555,31 @@ class QuickCreateTicketDialog(Component):
             dum_req.perm = req.perm
             ticket = Ticket(self.env)
             tm._populate(dum_req, ticket, False)
-            all_fields = dict([f['name'], self.add_prod_new_ticket_url(dum_req, f)]
+            all_fields = dict([f['name'], f]
                               for f in tm._prepare_fields(dum_req, ticket)
                               if f['type'] == 'select')
 
             product_field = all_fields.get('product')
             if product_field:
-                # Filter out products for which user doesn't have TICKET_CREATE
-                product_field['options'] = \
-                    [prefix for prefix in product_field['options']
-                     if req.perm.has_permission('TICKET_CREATE',
-                                                Neighborhood('product', prefix['value'])
-                                                    .child(None, None))]
-
+                # When at product scope, set the default selection to the
+                # product at current scope. When at global scope the default
+                # selection is determined by [ticket] default_product
                 if self.env.product and \
                         self.env.product.prefix in product_field['options']:
                     product_field['value'] = self.env.product.prefix
-                product_field['options_desc'] = [
-                    ProductEnvironment.lookup_env(self.env, p['value']).product.name
-                        for p in product_field['options']
-                ]
+                # Transform the options field to dictionary of product
+                # attributes and filter out products for which user doesn't
+                #  have TICKET_CREATE permission
+                product_field['options'] = [
+                    dict(value=p,
+                         new_ticket_url=dum_req.href.products(p, 'newticket'),
+                         description=ProductEnvironment.lookup_env(self.env, p)
+                                                       .product.name
+                    )
+                for p in product_field['options']
+                    if req.perm.has_permission('TICKET_CREATE',
+                                               Neighborhood('product', p)
+                                               .child(None, None))]
             else:
                 msg = _("Missing ticket field '%(field)s'.", field='product')
                 if ProductTicketModule is not None and \
@@ -665,18 +669,6 @@ class QuickCreateTicketDialog(Component):
                 self.log.exception("Failure sending notification on creation "
                                    "of ticket #%s: %s" % (t.id, e))
         return t['product'], t.id
-
-    def add_prod_new_ticket_url(self, req, fields):
-        if fields.get('name') == 'product':
-            options_with_attrs = []
-            for option in fields.get('options', []):
-                options_with_attrs.append({
-                    'value': option,
-                    'product_new_ticket_url': \
-                            req.href.products(option, 'newticket')
-                })
-            fields['options'] = options_with_attrs
-        return fields
 
 from pkg_resources import get_distribution
 application_version = get_distribution('BloodhoundTheme').version